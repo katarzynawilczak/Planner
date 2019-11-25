db91412ee2b1f979e66f74bd0be0f88d29403cfb
katarzynawilczak <kasia.wilczak@yahoo.com>
2019-11-25 22:08:55 +0100

    Menu Activity done

---
+++ app/src/main/java/com/example/planner/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.planner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToList(View view) {
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSchedule(View view) {
+        Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCalendar(View view) {
+        Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+        startActivity(intent);
+    }
+}
