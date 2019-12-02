fa10588ba30267df3e41cd87c004b6306bb455a3
agatasidlo <agata.sidlo@interia.pl>
2019-12-03 00:00:58 +0100

    Some parts of calendar

--- app/src/main/java/com/example/planner/CalendarActivity.java
+++ app/src/main/java/com/example/planner/CalendarActivity.java
@@ -3,12 +3,31 @@ package com.example.planner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
+    private TextView dataView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        dataView = (TextView) findViewById(R.id.currentData);
+        Date currentTime = Calendar.getInstance().getTime();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH).format(currentTime);
+        dataView.setText(currentDate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.calendar_menu, menu);
+        return true;
     }
 }
