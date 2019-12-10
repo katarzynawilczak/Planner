cf969b9d4ab1fe44e66f9f27c7e4e24bf45a30eb
katarzynawilczak <kasia.wilczak@yahoo.com>
2019-12-10 11:40:20 +0100

    calendar name days

--- app/src/main/java/com/example/planner/calendar/CalendarActivity.java
+++ app/src/main/java/com/example/planner/calendar/CalendarActivity.java
@@ -2,6 +2,7 @@ package com.example.planner.calendar;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,7 +14,20 @@ import com.example.planner.R;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -33,6 +47,7 @@ public class CalendarActivity extends AppCompatActivity {
     ArrayList<String> names;
     private Date visiblePosition = Calendar.getInstance().getTime();
     private MyDBHandlerCalendar myDBHandler;
+    private TextView nameDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +59,13 @@ public class CalendarActivity extends AppCompatActivity {
         String currentDate = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH).format(currentTime);
         dataView.setText(currentDate);
         setCurrentMonth(currentTime);
+
+        nameDay = findViewById(R.id.nameDay);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(currentTime);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        new JSONTask().execute("https://api.abalin.net/namedays?country=us&month="+month+"&day=" + day);
 
         setNotesInCalendar();
 
@@ -57,6 +79,13 @@ public class CalendarActivity extends AppCompatActivity {
                 List<Event> events = calendarView.getEvents(dateClicked);
                 if (!events.isEmpty())
                     Toast.makeText(CalendarActivity.this, "Note: "  + events.get(0).getData(), Toast.LENGTH_LONG).show();
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(dateClicked);
+                int month = cal.get(Calendar.MONTH) + 1;
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                new JSONTask().execute("https://api.abalin.net/namedays?country=us&month="+month+"&day=" + day);
+
             }
 
             @Override
@@ -125,5 +154,67 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         setNotesInCalendar();
+    }
+
+
+    private class JSONTask extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection conn = null;
+            BufferedReader reader = null;
+
+            try{
+                URL url = new URL(params[0]);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+
+                InputStream stream = conn.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+
+                String finalJSON =  buffer.toString();
+
+                JSONObject jsonObject = new JSONObject(finalJSON);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                JSONObject jsonDateAndNames = jsonArray.getJSONObject(0);
+                JSONObject jsonNames = jsonDateAndNames.getJSONObject("namedays");
+                String names = jsonNames.getString("us");
+                return "Name day: " + names;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if(conn != null){
+                    conn.disconnect();
+                }
+                try{
+                    if(reader != null){
+                        reader.close();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            nameDay.setText(result);
+        }
     }
 }
